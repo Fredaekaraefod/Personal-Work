fe157f8687c3cc98f787fd8edcd8e509d1aac4f9
Fredaekaraefod <fredaefod@gmail.com>
2022-09-09 17:44:15 +0100

    Hope this makes sense

---
+++ calculator.cpp
@@ -0,0 +1,46 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+  char op;
+  float num1, num2;
+
+  cout << "Enter operator:";
+  cout << "Operator \t Use \n" ;
+  cout << "+        \t Add \n" ;
+  cout << "-  \t Subtract \n " ;
+  cout << "*  \t Multiply \n " ;
+  cout << "/     \t Divide \n" ; 
+  cin >> op;
+
+  cout << "Enter two operands: ";
+  cin >> num1 >> num2;
+
+  switch(op) {
+
+    case '+':
+      cout << num1 << " + " << num2 << " = " << num1 + num2;
+      break;
+
+    case '-':
+      cout << num1 << " - " << num2 << " = " << num1 - num2;
+      break;
+
+    case '*':
+      cout << num1 << " * " << num2 << " = " << num1 * num2;
+      break;
+
+    case '/':
+      cout << num1 << " / " << num2 << " = " << num1 / num2;
+      break;
+
+    default:
+      // If the operator is other than +, -, * or /, error message is shown
+      cout << "Error! operator is not correct";
+      break;
+  }
+
+  return 0;
+}
+
