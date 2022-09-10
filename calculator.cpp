3a2d800be97db43cb7adefda9b9ca0f74860f4cc
Freda <101472460+Fredaekaraefod@users.noreply.github.com>
2022-09-10 17:13:20 +0100

    Added a few comments

--- calculator.cpp
+++ calculator.cpp
@@ -1,22 +1,31 @@
+// Program for creating a calculator
 # include <iostream>
 using namespace std;
 
 int main() {
-
-  char op;
+// Declairing the variable 
+  
+  char op; // this is short for operator fyi
   float num1, num2;
 
+  // Showing the output
+  
   cout << "Enter operator:";
   cout << "Operator \t Use \n" ;
   cout << "+        \t Add \n" ;
   cout << "-  \t Subtract \n " ;
   cout << "*  \t Multiply \n " ;
   cout << "/     \t Divide \n" ; 
+
+  //This is where the the user will input the operand
+  
   cin >> op;
 
   cout << "Enter two operands: ";
   cin >> num1 >> num2;
 
+  //This is where the switch command takes full effect
+  
   switch(op) {
 
     case '+':
